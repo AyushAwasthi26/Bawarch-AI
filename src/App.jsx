02e74f7a74c3cbd7316fbfd544c239916cf2f808
Ayush Awasthi <ayushawasthi382@gmail.com>
2026-07-12 12:34:32 +0530

    Added smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Head from './components/Header'
 import Body from './components/Body'
 
+// Adding smooth scroll using LENIS, just cause i want to and i have been awake for last 40 hours and i am maybe losing it, but idk f it, we Ball.
+import { ReactLenis } from 'lenis/react';
+
+// Required: Prevents browser default scroll bouncing and layout jumps
+import 'lenis/dist/lenis.css';
+
 export default function App(){
   return(
     <>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, syncTouch: true }}>
       <Head/>
       <Body/>
+    </ReactLenis>
     </>
   )
 }
